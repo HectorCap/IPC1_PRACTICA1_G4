6364e5751c5a864424ed8dd581139c3cdb39c29d
MarthinnChew <42487089+MarthinnChew@users.noreply.github.com>
2021-06-04 21:34:20 -0600

    iNGRESO DE MATRICES DE TXT A MATRICES CORRESPONDIENTES

--- [IPC1]G4_Practica1/src/ipc1/g4_practica1/IPC1G4_Practica1.java
+++ [IPC1]G4_Practica1/src/ipc1/g4_practica1/IPC1G4_Practica1.java
@@ -7,9 +7,36 @@ import java.io.FileReader;
 import java.util.Scanner;
 
 public class IPC1G4_Practica1 {
+
     //public static int Matriz[][][];//Esta hace referencia al guardado de cada una de las matrices
     public static Scanner entrada = new Scanner(System.in);
-    
+    public static double matrizA[][];
+    public static double matrizB[][];
+    public static double matrizC[][];
+    public static double matrizD[][];
+    public static double matrizE[][];
+    public static double matrizF[][];
+    public static double matrizG[][];
+    public static double matrizH[][];
+    public static double matrizI[][];
+    public static double matrizJ[][];
+    public static double matrizK[][];
+    public static double matrizL[][];
+    public static double matrizM[][];
+    public static double matrizN[][];
+    public static double matrizO[][];
+    public static double matrizP[][];
+    public static double matrizQ[][];
+    public static double matrizR[][];
+    public static double matrizS[][];
+    public static double matrizT[][];
+    public static double matrizU[][];
+    public static double matrizV[][];
+    public static double matrizW[][];
+    public static double matrizX[][];
+    public static double matrizY[][];
+    public static double matrizZ[][];
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int opcion = 0;
@@ -74,26 +101,28 @@ public class IPC1G4_Practica1 {
             }
         } while (opcion != 12);
     }
-    public static void getContentOfFile(String pathname) {
-    File archivo = null;
-    FileReader fr = null;
-    BufferedReader br = null;
 
-    try {
-        // Apertura del fichero y creacion de BufferedReader para poder
-        // hacer una lectura comoda (disponer del metodo readLine()).
-        archivo = new File(pathname);
-        fr = new FileReader(archivo);
-        br = new BufferedReader(fr);
-        // Lectura del fichero
-        String content = "";
-        String linea;
+    public static void getContentOfFile(String pathname) {
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File(pathname);
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+            // Lectura del fichero
+            String content = "";
+            String linea;
             //Cada linea leída del txt representa una matriz            
             while ((linea = br.readLine()) != null) {
-                
+
                 String[] nombre_datos = linea.split(":");
                 //nombre_datos[0] es el nombre de la matriz
-                System.out.println(nombre_datos[1].trim());// estan los datos de la matriz
+//                System.out.println(nombre_datos[1].trim());// estan los datos de la matriz
+                System.out.println(nombre_datos[0].trim());//estan los datos de los indices
 
                 if (nombre_datos[1].contains(";")) {
                     //La matriz posee varias filas, se puede hacer split
@@ -116,8 +145,121 @@ public class IPC1G4_Practica1 {
                             for (int j = 0; j < datosEnLaFila.length; j++) {
                                 Matriz[i][j] = Double.parseDouble(datosEnLaFila[j].trim());
                                 System.out.print("" + Matriz[i][j] + "\t");
+
                             }
-                             System.out.println("\t");
+
+                            System.out.println("\t");
+                            String letra = String.valueOf(nombre_datos[0]);//PARSEA EL INDICE PARA ENTRAR AL SWITCH Y LLENAR LA MATRIZ CORRESPONDIENTE
+
+                            switch (letra) {
+                                case "A":
+                                    matrizA = Matriz;
+
+                                    break;
+
+                                case "B":
+                                    matrizB = Matriz;
+
+                                    break;
+                                case "C":
+                                    matrizC = Matriz;
+
+                                    break;
+
+                                case "D":
+                                    matrizD = Matriz;
+
+                                    break;
+                                case "E":
+                                    matrizE = Matriz;
+
+                                    break;
+                                case "F":
+                                    matrizF = Matriz;
+
+                                    break;
+                                case "G":
+                                    matrizG = Matriz;
+
+                                    break;
+                                case "H":
+                                    matrizH = Matriz;
+
+                                    break;
+                                case "I":
+                                    matrizI = Matriz;
+
+                                    break;
+                                case "J":
+                                    matrizJ = Matriz;
+
+                                    break;
+                                case "K":
+                                    matrizK = Matriz;
+
+                                    break;
+                                case "L":
+                                    matrizL = Matriz;
+
+                                    break;
+                                case "M":
+                                    matrizM = Matriz;
+
+                                    break;
+                                case "N":
+                                    matrizN = Matriz;
+
+                                    break;
+                                case "O":
+                                    matrizO = Matriz;
+                                    break;
+                                case "P":
+                                    matrizP = Matriz;
+
+                                    break;
+                                case "Q":
+                                    matrizQ = Matriz;
+
+                                    break;
+                                case "R":
+                                    matrizR = Matriz;
+
+                                    break;
+                                case "S":
+                                    matrizS = Matriz;
+
+                                    break;
+                                case "T":
+                                    matrizT = Matriz;
+
+                                    break;
+                                case "U":
+                                    matrizU = Matriz;
+
+                                    break;
+                                case "V":
+                                    matrizV = Matriz;
+
+                                    break;
+                                case "W":
+                                    matrizW = Matriz;
+
+                                    break;
+                                case "X":
+                                    matrizX = Matriz;
+
+                                    break;
+                                case "Y":
+                                    matrizY = Matriz;
+
+                                    break;
+                                case "Z":
+                                    matrizZ = Matriz;
+
+                                    break;
+
+                            }
+
                         }
 
                     } else {
@@ -137,15 +279,15 @@ public class IPC1G4_Practica1 {
                 } else if (nombre_datos[1].contains(",")) {
                     //La matriz posee una fila y varias columnas
 
-                        String[] columnas = nombre_datos[1].split(",");
+                    String[] columnas = nombre_datos[1].split(",");
                         //Cada elemento del array es una columna de la matriz
 
-                        //Declarando la matriz
-                        double[][] Matriz = new double[1][columnas.length];
+                    //Declarando la matriz
+                    double[][] Matriz = new double[1][columnas.length];
 
-                        for (int i = 0; i < columnas.length; i++) {
-                            Matriz[0][i] = Double.parseDouble(columnas[i].trim());
-                        }
+                    for (int i = 0; i < columnas.length; i++) {
+                        Matriz[0][i] = Double.parseDouble(columnas[i].trim());
+                    }
 
                 } else {
                     //La matriz posee un único dato
@@ -155,31 +297,30 @@ public class IPC1G4_Practica1 {
                     Matriz[0] = Double.parseDouble(nombre_datos[1].trim());
                 }
             }
-       
-    } catch (Exception e) {
-        e.printStackTrace();
-    } finally {
-        // En el finally cerramos el fichero, para asegurarnos
-        // que se cierra tanto si todo va bien como si salta
-        // una excepcion.
-        try {
-            if (null != fr) {
-                fr.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
             }
-        } catch (Exception e2) {
-            e2.printStackTrace();
         }
+
     }
-  
-}
-    
+
     public static void Ruta() {//carga y guardado del contenido del Txt
-        System.out.print("Ingrese ruta del txt ");
+        System.out.println("Ingrese ruta del txt: ");
         String ruta = entrada.nextLine();
         getContentOfFile(ruta);
     }
-    
-    
+
     public static int[][] multiplicarMatriz(int[][] a, int[][] b) {
         int[][] c = new int[a.length][b[0].length];
         // se comprueba si las matrices se pueden multiplicar
@@ -198,6 +339,7 @@ public class IPC1G4_Practica1 {
          */
         return c;
     }
+
     public static int[][] sumarMatriz(int[][] matrizA, int[][] matrizB) {
         int[][] matrizResultado;
         int filasA = matrizA.length;
@@ -243,6 +385,7 @@ public class IPC1G4_Practica1 {
         }
         return matrizResultado;
     }
+
     public static int[][] restarMatriz(int[][] matrizA, int[][] matrizB) {
         int[][] matrizResultado;
         int filasA = matrizA.length;
@@ -267,6 +410,5 @@ public class IPC1G4_Practica1 {
 
         return matrizResultado;
     }
-    
-    
+
 }
